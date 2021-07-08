e612fb5a16ef7c7f0454a0cbc401fbf0e83a9ef0
Pascal Fischer <p-fischer@gmx.net>
2021-07-08 09:24:23 +0200

    add sale_price, cleanup

--- src/FondOfSpryker/Shared/GoogleMicrodata/GoogleMicrodataConstants.php
+++ src/FondOfSpryker/Shared/GoogleMicrodata/GoogleMicrodataConstants.php
@@ -15,6 +15,11 @@ interface GoogleMicrodataConstants
     public const TYPE_THING = 'Thing';
     public const SCHEMA_IN_STOCK = 'http://schema.org/InStock';
     public const SCHEMA_OUT_OF_STOCK = 'http://schema.org/OutOfStock';
+    public const PRODUCT_PRICE = 'price';
+    public const PRODUCT_SALE_PRICE = 'sale_price';
+    public const PRODUCT_CURRENCY = 'priceCurrency';
+    public const PRODUCT_URL = 'url';
+    public const PRODUCT_AVAILABILITY = 'availability';
     public const PRODUCT_ATTRIBUTE_IS_SOLD_OUT = 'is_sold_out';
     public const PRODUCT_ATTRIBUTE_SPECIAL_PRICE = 'special_price';
     public const PRODUCT_ATTRIBUTE_SPECIAL_PRICE_FROM = 'special_price_from';
